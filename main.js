1f9f0e779989a794c6f6e5671eb0c366b9747b56
sliver <125364267+slivermarkr@users.noreply.github.com>
2024-02-26 16:52:42 +0800

    adding functionality

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+const btn = document.querySelector('.add')
+const cardContainer = document.querySelector('.card-container');
+
 const myLibrary = [];
 
 function Book(title,author,pages,status) {
@@ -5,12 +8,27 @@ function Book(title,author,pages,status) {
  this.author = author;
  this.pages = pages;
  this.status = status;
+}
+Book.prototype.info = function() { 
+ return `${this.title} by ${this.author}, ${this.pages} pages, ${this.status}`}
 
- this.info = function() { 
- return `${this.title} by ${this.author}, ${this.pages} pages, ${this.status}`
-}
-}
+
 
 function addBookToLibrary() {
+ let title = prompt("Title:")
+ let author = prompt("Author:")
+ let pages = prompt("Pages:")
+ let status = prompt("Have you read it yet?")
 
+ let newBook = new Book(title,author,pages,status)
+ myLibrary.push(newBook);
 }
+btn.addEventListener('click',addBookToLibrary);
+
+
+
+myLibrary.forEach(book => {
+ for(let key in book) {
+  console.log(`${key}:${book[key]}`)
+ }
+})
