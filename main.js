61c5409998653847c5b9653ba7269a05c2779e12
sliver <125364267+slivermarkr@users.noreply.github.com>
2024-02-28 17:13:14 +0800

    getting the value of input

--- main.js
+++ main.js
@@ -1,9 +1,27 @@
+// create a dialog with a form that allows user to write the inputs
+
+//set the value of inputs to the Content on the div 
+
+//add card-class to display the input
+
+
+
 const addBtn = document.querySelector('.add')
 const cardContainer = document.querySelector('.card-container');
 const dialog = document.querySelector('#dialog');
+const inputs = document.querySelector(".form-container");
+const confirmBtn = dialog.querySelector('#confirmBtn');
 
-addBtn.addEventListener('click' , () => {
- dialog.showModal();
+let title = dialog.querySelector("#title");
+
+title.addEventListener('input', () => {
+ console.log(title.value)
+})
+
+
+confirmBtn.addEventListener('click' , (e) => {
+ e.preventDefault();
+ console.log("clicked");
 })
 const myLibrary = [];
 
@@ -15,40 +33,45 @@ function Book(title,author,pages,status) {
 }
 Book.prototype.info = function() { 
  return `${this.title} by ${this.author}, ${this.pages} pages, ${this.status}`}
+ 
+ function addBookToLibrary() { 
+  let newBook = new Book(title,author,pages,status)
+  myLibrary.push(newBook);
+  createCard()
+ }
+ // btn.addEventListener('click',addBookToLibrary);
+ 
+ 
+ 
+ function createCard() {
+  let card = document.createElement("div")
+  card.classList = "card";
+  cardContainer.appendChild(card);
+  let title = document.createElement("div");
+  let author = document.createElement("div");
+  let pages = document.createElement("div");
+  let status = document.createElement("div");
+  
+  myLibrary.forEach(book => {
+   for(let key in book) {
+    title.textContent = `Title:${book.title}`
+    card.appendChild(title);
+    author.textContent = `Author:${book.author}`
+    card.appendChild(author)
+    pages.textContent = `No. of Pages:${book.pages}`
+    card.appendChild(pages)
+    status.textContent = `Status:${book.status}`
+    card.appendChild(status)
+   }
+  })
+ }
 
-function addBookToLibrary() {
- let title = prompt("Title:")
- let author = prompt("Author:")
- let pages = prompt("Pages:")
- let status = prompt("Have you read it yet?")
-
- let newBook = new Book(title,author,pages,status)
- myLibrary.push(newBook);
- createCard()
-}
-// btn.addEventListener('click',addBookToLibrary);
-
-
-
-function createCard() {
- let card = document.createElement("div")
- card.classList = "card";
- cardContainer.appendChild(card);
- let title = document.createElement("div");
- let author = document.createElement("div");
- let pages = document.createElement("div");
- let status = document.createElement("div");
-
- myLibrary.forEach(book => {
-  for(let key in book) {
-   title.textContent = `Title:${book.title}`
-   card.appendChild(title);
-   author.textContent = `Author:${book.author}`
-   card.appendChild(author)
-   pages.textContent = `No. of Pages:${book.pages}`
-   card.appendChild(pages)
-   status.textContent = `Status:${book.status}`
-   card.appendChild(status)
-  }
+ 
+ addBtn.addEventListener('click' , () => {
+  dialog.showModal();
  })
-}
+
+
+ function getInputvalue(){
+
+ }
